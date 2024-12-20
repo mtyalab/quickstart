87c9094e82e90af7b6d2c5a8b467f4dc9be41818
Krista Mutyaba <mutyabakrista2016@gmail.com>
2024-12-20 12:40:31 +0300

    Update DTOs

--- src/main/java/com/quickstart/dto/LoginResponseDTO.java
+++ src/main/java/com/quickstart/dto/LoginResponseDTO.java
@@ -1,8 +1,14 @@
 package com.quickstart.dto;
 
-public record LoginResponseDTO(
-        String token,
-        UserDetailsDTO user
-) {
+import jakarta.validation.constraints.NotBlank;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
 
-}
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record LoginResponseDTO(
+        @NotBlank(message = "")
+        String token,
+        @NotBlank(message = "")
+        UserDetailsDTO user
+) {}
